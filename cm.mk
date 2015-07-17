0934fb5a08e4a797b872332f996fb7bf4e617c2c
David Müller <david.mueller1013@gmail.com>
2015-07-18 00:12:45 +0200

    PA

--- cm.mk
+++ cm.mk
@@ -1,7 +1,7 @@
 $(call inherit-product, device/samsung/lt03lte/full_lt03lte.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common PA stuff.
+$(call inherit-product, vendor/pa/config/common_full_phone.mk)
 
 PRODUCT_DEVICE := lt03lte
-PRODUCT_NAME := cm_lt03lte
+PRODUCT_NAME := pa_lt03lte
